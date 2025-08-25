b8e3b44e63e5c6c0a98dabebb4c9693a5e46adff
SodaK923 <k.sua923@gmail.com>
2025-08-25 06:26:31 +0000

    테스트

---
+++ src/pages/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <main>
+      <h1>Welcome to My Portfolio</h1>
+      <p>Thank you for visiting! Feel free to explore my projects and learn more about me.</p>
+    </main>
+  );
+}
+
+export default HomePage;
